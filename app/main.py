6e9547cc09e0d06878f64af868c90fdfc3245920
Nejm <nejmeddine.khechine@esprit.tn>
2021-05-21 03:50:12 +0100

    Messaging API

---
+++ app/main.py
@@ -0,0 +1,53 @@
+"""The Flask App."""
+
+# pylint: disable=broad-except
+
+from flask import Flask, abort, jsonify, request
+from rq.job import Job
+from rq import Worker, Queue, Connection
+from rq import Queue
+from .functions import some_long_function
+from .redis_resc import redis_conn, redis_queue
+import os
+app = Flask(__name__)
+
+
+@app.errorhandler(404)
+def resource_not_found(exception):
+    """Returns exceptions as part of a json."""
+    return jsonify(error=str(exception)), 404
+
+
+@app.route("/")
+def home():
+    """Show the app is working."""
+    return "Running!"
+
+def count_and_save_words(message):
+    return [message]
+#add Message
+@app.route('/api/message', methods=['PUT'])
+def addmessaage():
+    data = request.get_json()
+    message=data['message']
+    q = Queue(connection=redis_conn)
+    job = q.enqueue_call(
+            func=count_and_save_words, args=(message,), result_ttl=604800)
+    return "http://"+os.getenv("HOST", "127.0.0.1")+":"+os.getenv("PORT", "5000")+"/api/message/"+(job.get_id())
+
+#view Message
+@app.route("/api/message/<job_key>", methods=['GET'])
+def get_results(job_key):
+    job = Job.fetch(job_key, connection=redis_conn)
+    try:
+        if job.is_finished:
+            return str(job.result), 200
+        else:
+            return "Nay!", 202
+    except Exception as exception:
+        abort(404, description=exception)
+
+
+if __name__ == "__main__":
+    app.run(debug=False)
+
